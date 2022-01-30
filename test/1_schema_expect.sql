26bf8524b7f256c17b3ed5ea07007639613bbdec
Leon Huang <zlargon1988@gmail.com>
2022-01-30 00:16:49 -0500

    ✨ remove the invalid types

--- test/1_schema_expect.sql
+++ test/1_schema_expect.sql
@@ -27,7 +27,7 @@ CREATE SCHEMA public;
 -- Name: mpaa_rating; Type: TYPE; Schema: public; Owner: -
 --
 
-CREATE TYPE public.mpaa_rating AS ENUM (
+CREATE TYPE mpaa_rating AS ENUM (
     'G',
     'PG',
     'PG-13',
@@ -102,14 +102,14 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.customer (
-    customer_id integer DEFAULT nextval('public.customer_customer_id_seq'::regclass) NOT NULL,
+    customer_id integer,
     store_id smallint NOT NULL,
     first_name character varying(45) NOT NULL,
     last_name character varying(45) NOT NULL,
     email character varying(50),
     address_id smallint NOT NULL,
     activebool boolean DEFAULT true NOT NULL,
-    create_date date DEFAULT ('now'::text)::date NOT NULL,
+    create_date date,
     last_update timestamp without time zone DEFAULT now(),
     active integer
 );
@@ -142,7 +142,7 @@ CREATE SEQUENCE public.actor_actor_id_seq
 --
 
 CREATE TABLE public.actor (
-    actor_id integer DEFAULT nextval('public.actor_actor_id_seq'::regclass) NOT NULL,
+    actor_id integer,
     first_name character varying(45) NOT NULL,
     last_name character varying(45) NOT NULL,
     last_update timestamp without time zone DEFAULT now() NOT NULL
@@ -166,7 +166,7 @@ CREATE SEQUENCE public.category_category_id_seq
 --
 
 CREATE TABLE public.category (
-    category_id integer DEFAULT nextval('public.category_category_id_seq'::regclass) NOT NULL,
+    category_id integer,
     name character varying(25) NOT NULL,
     last_update timestamp without time zone DEFAULT now() NOT NULL
 );
@@ -189,18 +189,18 @@ CREATE SEQUENCE public.film_film_id_seq
 --
 
 CREATE TABLE public.film (
-    film_id integer DEFAULT nextval('public.film_film_id_seq'::regclass) NOT NULL,
+    film_id integer,
     title character varying(255) NOT NULL,
     description text,
-    release_year public.year,
+    release_year year,
     language_id smallint NOT NULL,
     rental_duration smallint DEFAULT 3 NOT NULL,
     rental_rate numeric(4,2) DEFAULT 4.99 NOT NULL,
     length smallint,
     replacement_cost numeric(5,2) DEFAULT 19.99 NOT NULL,
-    rating public.mpaa_rating DEFAULT 'G'::public.mpaa_rating,
+    rating mpaa_rating,
     last_update timestamp without time zone DEFAULT now() NOT NULL,
-    special_features text[],
+    special_features text_array,
     fulltext tsvector NOT NULL
 );
 
@@ -265,7 +265,7 @@ CREATE SEQUENCE public.address_address_id_seq
 --
 
 CREATE TABLE public.address (
-    address_id integer DEFAULT nextval('public.address_address_id_seq'::regclass) NOT NULL,
+    address_id integer,
     address character varying(50) NOT NULL,
     address2 character varying(50),
     district character varying(20) NOT NULL,
@@ -293,7 +293,7 @@ CREATE SEQUENCE public.city_city_id_seq
 --
 
 CREATE TABLE public.city (
-    city_id integer DEFAULT nextval('public.city_city_id_seq'::regclass) NOT NULL,
+    city_id integer,
     city character varying(50) NOT NULL,
     country_id smallint NOT NULL,
     last_update timestamp without time zone DEFAULT now() NOT NULL
@@ -317,7 +317,7 @@ CREATE SEQUENCE public.country_country_id_seq
 --
 
 CREATE TABLE public.country (
-    country_id integer DEFAULT nextval('public.country_country_id_seq'::regclass) NOT NULL,
+    country_id integer,
     country character varying(50) NOT NULL,
     last_update timestamp without time zone DEFAULT now() NOT NULL
 );
@@ -384,7 +384,7 @@ CREATE SEQUENCE public.inventory_inventory_id_seq
 --
 
 CREATE TABLE public.inventory (
-    inventory_id integer DEFAULT nextval('public.inventory_inventory_id_seq'::regclass) NOT NULL,
+    inventory_id integer,
     film_id smallint NOT NULL,
     store_id smallint NOT NULL,
     last_update timestamp without time zone DEFAULT now() NOT NULL
@@ -408,7 +408,7 @@ CREATE SEQUENCE public.language_language_id_seq
 --
 
 CREATE TABLE public.language (
-    language_id integer DEFAULT nextval('public.language_language_id_seq'::regclass) NOT NULL,
+    language_id integer,
     name character(20) NOT NULL,
     last_update timestamp without time zone DEFAULT now() NOT NULL
 );
@@ -452,7 +452,7 @@ CREATE SEQUENCE public.payment_payment_id_seq
 --
 
 CREATE TABLE public.payment (
-    payment_id integer DEFAULT nextval('public.payment_payment_id_seq'::regclass) NOT NULL,
+    payment_id integer,
     customer_id smallint NOT NULL,
     staff_id smallint NOT NULL,
     rental_id integer NOT NULL,
@@ -478,7 +478,7 @@ CREATE SEQUENCE public.rental_rental_id_seq
 --
 
 CREATE TABLE public.rental (
-    rental_id integer DEFAULT nextval('public.rental_rental_id_seq'::regclass) NOT NULL,
+    rental_id integer,
     rental_date timestamp without time zone NOT NULL,
     inventory_id integer NOT NULL,
     customer_id smallint NOT NULL,
@@ -522,7 +522,7 @@ CREATE SEQUENCE public.staff_staff_id_seq
 --
 
 CREATE TABLE public.staff (
-    staff_id integer DEFAULT nextval('public.staff_staff_id_seq'::regclass) NOT NULL,
+    staff_id integer,
     first_name character varying(45) NOT NULL,
     last_name character varying(45) NOT NULL,
     address_id smallint NOT NULL,
@@ -553,7 +553,7 @@ CREATE SEQUENCE public.store_store_id_seq
 --
 
 CREATE TABLE public.store (
-    store_id integer DEFAULT nextval('public.store_store_id_seq'::regclass) NOT NULL,
+    store_id integer,
     manager_staff_id smallint NOT NULL,
     address_id smallint NOT NULL,
     last_update timestamp without time zone DEFAULT now() NOT NULL
